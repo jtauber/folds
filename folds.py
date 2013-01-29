92809bae3b0ba3b26cab2529ce035c181d68821b
James Tauber <jtauber@jtauber.com>
2013-01-29 14:35:42 -0500

    started library based on notes

---
+++ folds.py
@@ -0,0 +1,30 @@
+from fractions import Fraction
+
+
+class Vertex:
+    def __init__(self, x, y, z):
+        self.coordinates = (x, y, z)
+
+
+def quadrance(vertex_1, vertex_2):
+    return sum((c1 - c2) ** 2 for c1, c2 in
+        zip(vertex_1.coordinates, vertex_2.coordinates))
+
+
+class Edge:
+    def __init__(self, vertex_1, vertex_2):
+        self.vertex_1 = vertex_1
+        self.vertex_2 = vertex_2
+    
+    def quadrance(self):
+        return quadrance(self.vertex_1, self.vertex_2)
+
+
+def rational_foot_of_altitude(vertex, edge):
+    x1, y1, z1 = edge.vertex_1.coordinates
+    x2, y2, z2 = edge.vertex_2.coordinates
+    x3, y3, z3 = vertex.coordinates
+    
+    t = Fraction((x1 - x2) * (x1 - x3) + (y1 - y2) * (y1 - y3) + (z1 - z2) * (z1 - z3), (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+    
+    return Vertex(x1 + (x2 - x1) * t, y1 + (y2 - y1) * t, z1 + (z2 - z1) * t)
