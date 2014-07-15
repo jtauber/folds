6f05596ed89b8d7f09e1dd6ed3df816c72ec10f5
James Tauber <jtauber@jtauber.com>
2014-07-15 00:55:10 -0400

    fixed stray # in comment

--- notes.py
+++ notes.py
@@ -4,8 +4,8 @@
 # We'll start with the notion of a vertex
 
 # Because we're moving vertices around, their coordinates are inherently
-# mutable # but we need references to them to build up larger structures so
-# we're going to use a class containing a 3-tuple for the current coordinates
+# mutable but we need references to them to build up larger structures so we're
+# going to use a class containing a 3-tuple for the current coordinates
 
 class Vertex:
     def __init__(self, x, y, z):
